c2fbf196d5d17ad8c3c2b96803a2c278ff9c6f2e
Kerem Güneş <k-gun@mail.com>
2019-01-27 19:59:38 +0300

    Add Objects\Pool object.

---
+++ src/Objects/Pool.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * MIT License <https://opensource.org/licenses/mit>
+ *
+ * Copyright (c) 2015 Kerem Güneş
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+declare(strict_types=1);
+
+namespace Froq\Util\ObjectsPattern;
+
+/**
+ * @package    Froq
+ * @subpackage Froq\Util
+ * @object     Froq\Util\ObjectsPattern\Pool
+ * @author     Kerem Güneş <k-gun@mail.com>
+ * @since      3.0
+ */
+abstract class Pool
+{
+    /**
+     * Objects.
+     * @var array
+     */
+    private $objects = [];
+
+    /**
+     * Has.
+     * @param  int|string|object $id
+     * @return bool
+     */
+    public final function has($id): bool
+    {
+        if (is_object($id)) {
+            $id = spl_object_hash($id);
+            foreach ($this->objects as $object) {
+                if ($object[0] == $id) { return true; }
+            }
+            return false;
+        }
+
+        return isset($this->objects[$id]);
+    }
+
+    /**
+     * Set.
+     * @param  int|string $id
+     * @param  object     $object
+     * @return void
+     */
+    public final function set($id, object $object): void
+    {
+        $this->objects[$id] = [spl_object_hash($object), $object];
+    }
+
+    /**
+     * Get.
+     * @param  int|string|object $id
+     * @return ?object
+     */
+    public final function get($id): ?object
+    {
+        if (is_object($id)) {
+            $id = spl_object_hash($id);
+            foreach ($this->objects as $object) {
+                if ($object[0] == $id) { return $object[1]; }
+            }
+            return null;
+        }
+
+        return $this->objects[$id][1] ?? null;
+    }
+
+    /**
+     * Remove.
+     * @param  int|string|object $id
+     * @return void
+     */
+    public final function remove($id): void
+    {
+        if (is_object($id)) {
+            $id = spl_object_hash($id);
+            foreach ($this->objects as $i => $object) {
+                if ($object[0] == $id) { unset($this->objects[$i]); break; }
+            }
+        } else {
+            unset($this->objects[$id]);
+        }
+    }
+
+    /**
+     * Add.
+     * @param  int|string $id
+     * @param  object     $object
+     * @return void
+     * @throws Froq\Util\ObjectsPattern\PoolException
+     */
+    public final function add($id, object $object): void
+    {
+        if ($this->has($id)) {
+            throw new PoolException("Object already in pool with id '{$id}'");
+        }
+
+        $this->set($id, $object);
+    }
+
+    /**
+     * Size.
+     * @return int
+     */
+    public final function size(): int
+    {
+        return count($this->objects);
+    }
+
+    /**
+     * Create object.
+     * @param  int|string $id
+     * @return any
+     */
+    abstract public function createObject($id);
+}
+
+/**
+ * @package    Froq
+ * @subpackage Froq\Util
+ * @object     Froq\Util\ObjectsPattern\PoolException
+ * @author     Kerem Güneş <k-gun@mail.com>
+ * @since      3.0
+ */
+final class PoolException extends \Exception
+{}
