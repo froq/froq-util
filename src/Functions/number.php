175c47d2fdb09971bcab54804f4ec165a74a9e88
Kerem Güneş <k-gun@mail.com>
2019-01-30 14:29:44 +0300

    Add int_rand(),float_rand().

--- src/Functions/number.php
+++ src/Functions/number.php
@@ -117,3 +117,23 @@ function is_unsigned($input): bool
 {
     return Numbers::isUnsigned($input);
 }
+
+/**
+ * Int rand.
+ * @param  bool $signed
+ * @return int
+ */
+function int_rand(bool $signed = false): int
+{
+    return ($ret = random_int(0, PHP_INT_MAX)) && $signed ? -$ret : $ret;
+}
+
+/**
+ * Float rand.
+ * @param  bool $signed
+ * @return float
+ */
+function float_rand(bool $signed = false): float
+{
+    return ($ret = lcg_value()) && $signed ? -$ret : $ret;
+}
