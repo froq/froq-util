2d6b1c81436208b863e2b8391a1e210df804c90a
Kerem Güneş <qeremy@gmail.com>
2020-01-30 03:12:38 +0300

    Update namespace froq\common\StaticClass => froq\common\objects\StaticClass.

--- src/Objects.php
+++ src/Objects.php
@@ -26,7 +26,7 @@ declare(strict_types=1);
 
 namespace froq\util;
 
-use froq\common\StaticClass;
+use froq\common\objects\StaticClass;
 use froq\common\exceptions\InvalidArgumentException;
 use ReflectionClass;
 
