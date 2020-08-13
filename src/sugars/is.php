398cc41cbb62e1ca8f28836f952b286043201ec6
Kerem Güneş <qeremy@gmail.com>
2020-08-13 20:32:38 +0300

    Nope..

--- src/sugars/is.php
+++ src/sugars/is.php
@@ -139,7 +139,7 @@ function is_class_method($in, string $name = null): bool
  * @param  string|object $in
  * @param  string        $name
  * @return bool
- * @since  3.0 ( actually)
+ * @since  3.0 (4.3.4 actually)
  */
 function is_class_property($in, string $name): bool
 {
