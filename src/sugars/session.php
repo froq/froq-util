9d64df2e918583647bb0a877620667fb37dc367f
Kerem Güneş <qeremy@gmail.com>
2020-12-20 09:38:34 +0300

    sugars.session: apply unions & null stuff, doc-ups.

--- src/sugars/session.php
+++ src/sugars/session.php
@@ -14,7 +14,8 @@ if (!class_exists(App::class, false)) {
 }
 
 /**
- * Session.
+ * Set/get a session variable or get session object.
+ *
  * @param  string|array|null $key
  * @param  any|null          $value
  * @return any|null|froq\session\Session
@@ -35,9 +36,10 @@ function session($key = null, $value = null)
 }
 
 /**
- * Session flash.
+ * Set/get session flash.
+ *
  * @param  any|null $message
- * @return ?any
+ * @return any|null
  */
 function session_flash($message = null)
 {
@@ -54,77 +56,84 @@ function session_flash($message = null)
 }
 
 /**
- * Session array.
- * @return ?array
+ * Get session as array.
+ *
+ * @return array|null
  */
-function session_array(): ?array
+function session_array(): array|null
 {
-    return ($session = session()) ? $session->toArray() : null;
+    return session()?->toArray();
 }
 
 /**
- * Session has.
+ * Check whether session has an variable.
+ *
  * @param  string $key
- * @return ?bool
+ * @return bool|null
  */
-function session_has(string $key): ?bool
+function session_has(string $key): bool|null
 {
-    return ($session = session()) ? $session->has($key) : null;
+    return session()?->has($key);
 }
 
 /**
- * Session set.
+ * Set a session variable.
+ *
  * @param  string|array $key
  * @param  any|null     $value
- * @return ?bool
+ * @return bool|null
  */
-function session_set($key, $value = null): ?bool
+function session_set($key, $value = null): bool|null
 {
-    return ($session = session()) ? $session->set($key, $value) != null : null;
+    return session()?->set($key, $value) ? true : null;
 }
 
 /**
- * Session get.
+ * Get a session variable.
+ *
  * @param  string|array $key
  * @param  any|null     $value
  * @param  bool         $remove
- * @return ?any
+ * @return any|null
  */
 function session_get($key, $default = null, bool $remove = false)
 {
-    return ($session = session()) ? $session->get($key, $default, $remove) : null;
+    return session()?->get($key, $default, $remove);
 }
 
 /**
- * Session remove.
+ * Remove a session variable.
+ *
  * @param  string|array $key
- * @return ?bool
+ * @return bool|null
  */
-function session_remove($key): ?bool
+function session_remove($key): bool|null
 {
-    return ($session = session()) ? $session->remove($key) : null;
+    return session()?->remove($key);
 }
 
 /**
  * Start session.
- * @return ?bool
+ *
+ * @return bool|null
  */
-function start_session(): ?bool
+function start_session(): bool|null
 {
-    return ($session = session()) ? $session->start() : null;
+    return session()?->start();
 }
 
 /**
  * End session.
- * @return ?bool
+ *
+ * @return bool|null
  */
-function end_session(): ?bool
+function end_session(): bool|null
 {
-    return ($session = session()) ? $session->end() : null;
+    return session()?->end();
 }
 
 /**
- * Generate a CSRF token.
+ * Generate a CSRF token for a form.
  *
  * @param  string $form
  * @param  string $token
@@ -133,11 +142,11 @@ function end_session(): ?bool
  */
 function generate_csrf_token(string $form): string|null
 {
-    return ($session = session()) ? $session->generateCsrfToken() : null;
+    return session()?->generateCsrfToken($form);
 }
 
 /**
- * Validate a CSRF token.
+ * Validate a CSRF token for a form.
  *
  * @param  string $form
  * @param  string $token
@@ -146,5 +155,5 @@ function generate_csrf_token(string $form): string|null
  */
 function validate_csrf_token(string $form, string $token): bool|null
 {
-    return ($session = session()) ? $session->validateCsrfToken($form, $token) : null;
+    return session()?->validateCsrfToken($form, $token);
 }
