dc36d1c3ae22a821379d2716ee63927ec83884e3
Kerem Güneş <qeremy@gmail.com>
2022-01-20 01:52:18 +0300

    Move common.object.StaticClass & common.trait.StaticClassTrait into.

--- src/sugars-class.php
+++ src/sugars-class.php
@@ -3,3 +3,4 @@
 require 'sugars-class/error.php';
 require 'sugars-class/mapset.php';
 require 'sugars-class/reflection.php';
+require 'sugars-class/static.php';
---
+++ src/sugars-class/static.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-util
+ */
+declare(strict_types=1);
+
+/**
+ * Static Class.
+ *
+ * Represents an uninitializable static class that forbid initializations of the extender classes.
+ * We wish it was part of PHP but not (@see http://wiki.php.net/rfc/static-classes).
+ *
+ * @package froq\util
+ * @object  StaticClass
+ * @author  Kerem Güneş
+ * @since   4.0, 6.0 Moved from "froq\common\object" stuff.
+ * @note    Not abstract'ed, letting the error in constructor.
+ */
+class StaticClass
+{
+    use StaticClassTrait;
+}
+
+/**
+ * Static Class Trait.
+ *
+ * Represents a trait entity which is able to forbid initialions on user object.
+ *
+ * @package froq\util
+ * @object  StaticClassTrait
+ * @author  Kerem Güneş
+ * @since   4.3, 6.0 Moved from "froq\common\object" stuff.
+ */
+trait StaticClassTrait
+{
+    /**
+     * Constructor.
+     *
+     * @throws Error
+     */
+    public final function __construct()
+    {
+        throw new Error('Cannot initialize static class ' . static::class);
+    }
+}
