755e1b021c97b19d82880b2d29de805bc87ad991
Kerem Güneş <qeremy@gmail.com>
2022-02-05 05:10:37 +0300

    Add "sugars-class/trace" stuff.

--- src/sugars-class.php
+++ src/sugars-class.php
@@ -10,6 +10,7 @@ require 'sugars-class/mapset.php';
 require 'sugars-class/reflection.php';
 require 'sugars-class/regexp.php';
 require 'sugars-class/type.php';
+require 'sugars-class/trace.php';
 require 'sugars-class/xclass.php';
 require 'sugars-class/xobject.php';
 require 'sugars-class/xstring.php';
---
+++ src/sugars-class/trace.php
@@ -0,0 +1,461 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-util
+ */
+declare(strict_types=1);
+
+/**
+ * Trace.
+ *
+ * A class for playing with traces in OOP-way.
+ *
+ * @package froq\util
+ * @object  Trace
+ * @author  Kerem Güneş
+ * @since   6.0
+ */
+final class Trace implements Countable, IteratorAggregate, ArrayAccess
+{
+    /** @var array */
+    public readonly array $stack;
+
+    /**
+     * Constructor.
+     *
+     * @param array|null $stack
+     * @param int        $options
+     * @param int        $limit
+     */
+    public function __construct(array $stack = null, int $options = 0, int $limit = 0)
+    {
+        $stack ??= get_trace($options, $limit);
+
+        $this->stack = $stack;
+    }
+
+    /** @magic */
+    public function __debugInfo(): array
+    {
+        return $this->stack;
+    }
+
+    /**
+     * Check an entry.
+     *
+     * @param  int  $index
+     * @return bool
+     */
+    public function has(int $index): bool
+    {
+        return isset($this->stack[$index]);
+    }
+
+    /**
+     * Get an entry.
+     *
+     * @param  int $index
+     * @return TraceEntry|null
+     */
+    public function get(int $index): TraceEntry|null
+    {
+        $entry = value($this->stack, $index);
+
+        return $entry ? new TraceEntry($entry) : null;
+    }
+
+    /**
+     * Get first entry.
+     *
+     * @return TraceEntry|null
+     */
+    public function getFirst(): TraceEntry|null
+    {
+        $entry = first($this->stack) ?: null;
+
+        return $entry ? new TraceEntry($entry) : null;
+    }
+
+    /**
+     * Get last entry.
+     *
+     * @return TraceEntry|null
+     */
+    public function getLast(): TraceEntry|null
+    {
+        $entry = last($this->stack) ?: null;
+
+        return $entry ? new TraceEntry($entry) : null;
+    }
+
+    /**
+     * @aliasOf getFirst()
+     */
+    public function first()
+    {
+        return $this->getFirst();
+    }
+
+    /**
+     * @aliasOf getLast()
+     */
+    public function last()
+    {
+        return $this->getLast();
+    }
+
+    /**
+     * Get a reversed trace.
+     *
+     * @return Trace
+     */
+    public function reverse(): Trace
+    {
+        return new Trace(array_reverse($this->stack));
+    }
+
+    /**
+     * @inheritDoc Countable
+     */
+    public function count(): int
+    {
+        return count($this->stack);
+    }
+
+    /**
+     * @inheritDoc IteratorAggregate
+     */ #[\ReturnTypeWillChange]
+    public function getIterator(): iterable
+    {
+        foreach ($this->stack as $entry) {
+            yield new TraceEntry($entry);
+        }
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     */
+    public function offsetExists(mixed $index): bool
+    {
+        return $this->has($index);
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     */
+    public function offsetGet(mixed $index): TraceEntry|null
+    {
+        return $this->get($index);
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     * @throws Error
+     * @notImplemented
+     */
+    public function offsetSet(mixed $index, mixed $_): never
+    {
+        throw new Error('Not implemented');
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     * @throws Error
+     * @notImplemented
+     */
+    public function offsetUnset(mixed $index): never
+    {
+        throw new Error('Not implemented');
+    }
+}
+
+/**
+ * Trace Entry.
+ *
+ * An internal class for Trace entries.
+ *
+ * @package froq\util
+ * @object  TraceEntry
+ * @author  Kerem Güneş
+ * @since   6.0
+ * @internal
+ */
+class TraceEntry implements ArrayAccess
+{
+    /** @var array */
+    public readonly array $data;
+
+    /**
+     * Constructor.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /** @magic */
+    public function __debugInfo(): array
+    {
+        return $this->data;
+    }
+
+    /** @magic */
+    public function __set(string $key, mixed $_): never
+    {
+        throw new Error('Not implemented');
+    }
+
+    /** @magic */
+    public function __get(string $key): mixed
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Check a field.
+     *
+     * @param  string $key
+     * @return bool
+     */
+    public function has(string $key): bool
+    {
+        return isset($this->data[$key]);
+    }
+
+    /**
+     * Get a field.
+     *
+     * @param  string $key
+     * @return mixed
+     */
+    public function get(string $key): mixed
+    {
+        return value($this->data, $key);
+    }
+
+    /**
+     * Get field.
+     *
+     * @param  string     $key
+     * @param  mixed|null $default
+     * @return mixed
+     */
+    public function getField(string $key, mixed $default = null): mixed
+    {
+        // When ref'ed  "Cannot modify readonly property TraceEntry::$data" error..
+        $data = $this->data;
+
+        return array_select($data, $key, default: $default);
+    }
+
+    /**
+     * Get fields.
+     *
+     * @param  array      $keys
+     * @param  mixed|null $default
+     * @return mixed
+     */
+    public function getFields(array $keys, mixed $default = null): mixed
+    {
+        // When ref'ed  "Cannot modify readonly property TraceEntry::$data" error..
+        $data = $this->data;
+
+        return array_select($data, $keys, default: $default);
+    }
+
+    /**
+     * @aliasOf getField()
+     */
+    public function field(...$args)
+    {
+        return $this->getField(...$args);
+    }
+
+    /**
+     * @aliasOf getFields()
+     */
+    public function fields(...$args)
+    {
+        return $this->getFields(...$args);
+    }
+
+    /**
+     * Get file.
+     *
+     * @return string
+     */
+    public function file(): string
+    {
+        return $this->getField('file');
+    }
+
+    /**
+     * Get line.
+     *
+     * @return int
+     */
+    public function line(): int
+    {
+        return $this->getField('line');
+    }
+
+    /**
+     * Get method.
+     *
+     * @return string|null
+     */
+    public function class(): string|null
+    {
+        return $this->getField('class');
+    }
+
+    /**
+     * Get method.
+     *
+     * @return string|null
+     */
+    public function method(): string|null
+    {
+        return $this->getField('method');
+    }
+
+    /**
+     * Get method type.
+     *
+     * @return string|null
+     */
+    public function methodType(): string|null
+    {
+        return $this->getField('methodType');
+    }
+
+    /**
+     * Get call.
+     *
+     * @return string
+     */
+    public function call(): string
+    {
+        return $this->callPath(true);
+    }
+
+    /**
+     * Get caller.
+     *
+     * @return string|null
+     */
+    public function caller(): string|null
+    {
+        return $this->getField('caller');
+    }
+
+    /**
+     * Get callee.
+     *
+     * @return string|null
+     */
+    public function callee(): string|null
+    {
+        return $this->getField('callee');
+    }
+
+    /**
+     * Get call path.
+     *
+     * @param  bool $full
+     * @return string
+     */
+    public function callPath(bool $full = false): string
+    {
+        $ret = $this->getField('callPath');
+
+        if ($full) {
+            [$class, $function, $type] = $this->getFields(['class', 'function', 'type']);
+            if ($class) {
+                $ret .= ' => '. $class . $type . $function .'()';
+            } else {
+                $ret .= ' => '. $function .'()';
+            }
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Get object.
+     *
+     * @return object|null
+     */
+    public function object(): object|null
+    {
+        return $this->getField('object');
+    }
+
+    /**
+     * Get function.
+     *
+     * @return string
+     */
+    public function function(): string
+    {
+        return $this->getField('function');
+    }
+
+    /**
+     * Get function type.
+     *
+     * @return string|null
+     */
+    public function functionType(): string|null
+    {
+        return $this->getField('type');
+    }
+
+    /**
+     * Get function arguments.
+     *
+     * @return array|null
+     */
+    public function functionArguments(): array|null
+    {
+        return $this->getField('args');
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     */
+    public function offsetExists(mixed $key): bool
+    {
+        return $this->has($key);
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     */
+    public function offsetGet(mixed $key): mixed
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     * @throws Error
+     * @notImplemented
+     */
+    public function offsetSet(mixed $key, mixed $_): never
+    {
+        throw new Error('Not implemented');
+    }
+
+    /**
+     * @inheritDoc ArrayAccess
+     * @throws Error
+     * @notImplemented
+     */
+    public function offsetUnset(mixed $key): never
+    {
+        throw new Error('Not implemented');
+    }
+}
