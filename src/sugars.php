5ce255d29cb418271a12aa043512ce8c7e100e02
Kerem Güneş <qeremy@gmail.com>
2022-02-24 05:50:44 +0300

    Doc-up.

--- src/sugars.php
+++ src/sugars.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 use froq\util\{Util, Arrays, Objects, Numbers, Strings};
 use froq\util\misc\System;
 
-// Load consts & classes.
+// Load base stuff.
 require 'sugars-const.php';
 require 'sugars-class.php';
 require 'sugars-function.php';
