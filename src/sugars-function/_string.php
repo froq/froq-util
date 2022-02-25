7ec69e2059a8dcac521604f764443289ba425ade
Kerem Güneş <qeremy@gmail.com>
2022-02-25 06:46:32 +0300

    * sugars-function/_string: update argument names, optimize, make-up, doc-up.

--- src/sugars-function/_string.php
+++ src/sugars-function/_string.php
@@ -20,53 +20,53 @@ function strsfx(...$args) { return str_has_suffix(...$args);  } // Search suffix
  * Shorter stuff in multi-byte style.
  * @since 3.0, 5.0
  */
-function upper(string $in): string { return mb_strtoupper($in); }
-function lower(string $in): string { return mb_strtolower($in); }
+function upper(string $string): string { return mb_strtoupper($string); }
+function lower(string $string): string { return mb_strtolower($string); }
 
 /**
  * Sub a string with given start/length in multi-byte style.
  *
- * @param  string   $str
+ * @param  string   $string
  * @param  int      $start
  * @param  int|null $length
  * @return string
  * @since  4.0
  */
-function strsub(string $str, int $start, int $length = null): string
+function strsub(string $string, int $start, int $length = null): string
 {
-    return mb_substr($str, $start, $length);
+    return mb_substr($string, $start, $length);
 }
 
 /**
  * Cut a string with given length in multi-byte style.
  *
- * @param  string $str
+ * @param  string $string
  * @param  int    $length
  * @return string
  * @since  4.0
  */
-function strcut(string $str, int $length): string
+function strcut(string $string, int $length): string
 {
-    return ($length >= 0) ? mb_substr($str, 0, $length) : mb_substr($str, $length);
+    return ($length >= 0) ? mb_substr($string, 0, $length) : mb_substr($string, $length);
 }
 
 /**
  * Cut a string before given search position with/without given length, or return '' if no search found.
  *
- * @param  string   $str
- * @param  string   $src
+ * @param  string   $string
+ * @param  string   $search
  * @param  int|null $length
- * @param  int|null $offset
  * @param  bool     $icase
+ * @param  int      $offset
  * @return string
  * @since  4.0
  */
-function strbcut(string $str, string $src, int $length = null, int $offset = null, bool $icase = false): string
+function strbcut(string $string, string $search, int $length = null, bool $icase = false, int $offset = 0): string
 {
-    $pos = !$icase ? mb_strpos($str, $src, $offset ?? 0) : mb_stripos($str, $src, $offset ?? 0);
+    $pos = !$icase ? mb_strpos($string, $search, $offset) : mb_stripos($string, $search, $offset);
 
     if ($pos !== false) {
-        $ret = mb_substr($str, 0, $pos); // Before (b).
+        $ret = mb_substr($string, 0, $pos);
         return $length ? strcut($ret, $length) : $ret;
     }
 
@@ -76,20 +76,20 @@ function strbcut(string $str, string $src, int $length = null, int $offset = nul
 /**
  * Cut a string after given search position with/without given length, or return '' if no search found.
  *
- * @param  string   $str
- * @param  string   $src
+ * @param  string   $string
+ * @param  string   $search
  * @param  int|null $length
- * @param  int|null $offset
+ * @param  int      $offset
  * @param  bool     $icase
  * @return string
  * @since  4.0
  */
-function stracut(string $str, string $src, int $length = null, int $offset = null, bool $icase = false): string
+function stracut(string $string, string $search, int $length = null, bool $icase = false, int $offset = 0): string
 {
-    $pos = !$icase ? mb_strpos($str, $src, $offset ?? 0) : mb_stripos($str, $src, $offset ?? 0);
+    $pos = !$icase ? mb_strpos($string, $search, $offset) : mb_stripos($string, $search, $offset);
 
     if ($pos !== false) {
-        $ret = mb_substr($str, $pos + mb_strlen($src)); // After (a).
+        $ret = mb_substr($string, $pos + mb_strlen($search));
         return $length ? strcut($ret, $length) : $ret;
     }
 
@@ -99,56 +99,62 @@ function stracut(string $str, string $src, int $length = null, int $offset = nul
 /**
  * Check whether a string has given search part or not with case-intensive option.
  *
- * @param  string $str
- * @param  string $src
+ * @param  string $string
+ * @param  string $search
  * @param  bool   $icase
  * @return bool
  * @since  4.0
  */
-function str_has(string $str, string $src, bool $icase = false): bool
+function str_has(string $string, string $search, bool $icase = false): bool
 {
-    return !$icase ? str_contains($str, $src) : mb_stripos($str, $src) !== false;
+    return !$icase ? str_contains($string, $search) : (
+        mb_stripos($string, $search) !== false
+    );
 }
 
 /**
  * Check whether a string has given prefix or not with case-intensive option.
  *
- * @param  string $str
- * @param  string $src
+ * @param  string $string
+ * @param  string $search
  * @param  bool   $icase
  * @return bool
  * @since  4.0
  */
-function str_has_prefix(string $str, string $src, bool $icase = false): bool
+function str_has_prefix(string $string, string $search, bool $icase = false): bool
 {
-    return !$icase ? str_starts_with($str, $src) : mb_stripos($str, $src) === 0;
+    return !$icase ? str_starts_with($string, $search) : (
+        mb_stripos($string, $search) === 0
+    );
 }
 
 /**
  * Check whether a string has given suffix or not with case-intensive option.
  *
- * @param  string $str
- * @param  string $src
+ * @param  string $string
+ * @param  string $search
  * @param  bool   $icase
  * @return bool
  * @since  4.0
  */
-function str_has_suffix(string $str, string $src, bool $icase = false): bool
+function str_has_suffix(string $string, string $search, bool $icase = false): bool
 {
-    return !$icase ? str_ends_with($str, $src) : mb_strripos($str, $src) === mb_strlen($str) - mb_strlen($src);
+    return !$icase ? str_ends_with($string, $search) : (
+        mb_strripos($string, $search) === mb_strlen($string) - mb_strlen($search)
+    );
 }
 
 /**
  * Randomize given string, return a subpart when length given.
  *
- * @param  string   $str
+ * @param  string   $string
  * @param  int|null $length
  * @return string
  * @since  4.9
  */
-function str_rand(string $str, int $length = null): string
+function str_rand(string $string, int $length = null): string
 {
-    $tmp = array_shuffle(mb_str_split($str), false);
+    $tmp = array_shuffle(mb_str_split($string), false);
 
     if ($length) {
         $tmp = array_slice($tmp, 0, abs($length));
@@ -160,16 +166,16 @@ function str_rand(string $str, int $length = null): string
 /**
  * Chunk given string properly in multi-byte style.
  *
- * @param  string $str
+ * @param  string $string
  * @param  int    $length
  * @param  string $separator
  * @param  bool   $join
  * @return string|array
  * @since  5.31
  */
-function str_chunk(string $str, int $length = 76, string $separator = "\r\n", bool $join = true): string|array
+function str_chunk(string $string, int $length = 76, string $separator = "\r\n", bool $join = true): string|array
 {
-    $ret = array_chunk(mb_str_split($str), abs($length));
+    $ret = array_chunk(mb_str_split($string), abs($length));
 
     if ($join) {
         $ret = array_reduce($ret, fn($s, $ss) => $s .= join($ss) . $separator);
@@ -181,25 +187,21 @@ function str_chunk(string $str, int $length = 76, string $separator = "\r\n", bo
 /**
  * Concat given string with others.
  *
- * @param  string    $str
- * @param  mixed  ...$strs
+ * @param  string               $string
+ * @param  string|Stringable ...$strings
  * @return string
  * @since  5.31
  */
-function str_concat(string $str, mixed ...$strs): string
+function str_concat(string $string, string|Stringable ...$strings): string
 {
-    if (!$strs) {
-        return $str;
-    }
-
-    return $str . join($strs);
+    return $string . join($strings);
 }
 
 /**
  * Compare two string inputs.
  *
- * @param  string      $str1
- * @param  string      $str2
+ * @param  string      $string1
+ * @param  string      $string2
  * @param  bool        $icase
  * @param  int|null    $length
  * @param  string|null $locale
@@ -207,92 +209,93 @@ function str_concat(string $str, mixed ...$strs): string
  * @return int
  * @since  5.26
  */
-function str_compare(string $str1, string $str2, bool $icase = false, int $length = null, string $locale = null, string $encoding = null): int
+function str_compare(string $string1, string $string2, bool $icase = false, int $length = null, string $locale = null,
+    string $encoding = null): int
 {
     return ($locale !== null)
-         ? Strings::compareLocale($str1, $str2, $locale)
-         : Strings::compare($str1, $str2, $icase, $length, $encoding);
+         ? Strings::compareLocale($string1, $string2, $locale)
+         : Strings::compare($string1, $string2, $icase, $length, $encoding);
 }
 
 /**
  * Convert a multi-byte string's first character to upper-case.
  *
- * @param  string      $str
+ * @param  string      $string
  * @param  bool        $tr
  * @param  string|null $encoding
  * @return string
  * @since  5.0
  */
-function mb_ucfirst(string $str, bool $tr = false, string $encoding = null): string
+function mb_ucfirst(string $string, bool $tr = false, string $encoding = null): string
 {
-    if ($str == '') {
-        return $str;
+    if ($string == '') {
+        return '';
     }
 
-    $first = mb_substr($str, 0, 1, $encoding);
+    $first = mb_substr($string, 0, 1, $encoding);
     if ($tr && $first == 'i') {
         $first = 'İ';
     }
 
-    return mb_strtoupper($first, $encoding) . mb_substr($str, 1, null, $encoding);
+    return mb_strtoupper($first, $encoding) . mb_substr($string, 1, null, $encoding);
 }
 
 /**
  * Convert a multi-byte string's first character to lower-case.
  *
- * @param  string      $str
+ * @param  string      $string
  * @param  bool        $tr
  * @param  string|null $encoding
  * @return string
  * @since  5.0
  */
-function mb_lcfirst(string $str, bool $tr = false, string $encoding = null): string
+function mb_lcfirst(string $string, bool $tr = false, string $encoding = null): string
 {
-    if ($str == '') {
-        return $str;
+    if ($string == '') {
+        return '';
     }
 
-    $first = mb_substr($str, 0, 1, $encoding);
+    $first = mb_substr($string, 0, 1, $encoding);
     if ($tr && $first == 'I') {
         $first = 'ı';
     }
 
-    return mb_strtolower($first, $encoding) . mb_substr($str, 1, null, $encoding);
+    return mb_strtolower($first, $encoding) . mb_substr($string, 1, null, $encoding);
 }
 
 /**
  * Reverse a multi-byte string.
  *
- * @param  string      $str
+ * @param  string      $string
  * @param  string|null $encoding
  * @return string
  * @since  6.0
  */
-function mb_strrev(string $str, string $encoding = null): string
+function mb_strrev(string $string, string $encoding = null): string
 {
-    if ($str == '') {
-        return $str;
+    if ($string == '') {
+        return '';
     }
 
-    return join(array_reverse(mb_str_split($str, 1, $encoding)));
+    return join(array_reverse(mb_str_split($string, 1, $encoding)));
 }
 
 /**
  * Get a character by given index in multi-byte style.
  *
- * @param  string      $str
+ * @param  string      $string
  * @param  int         $index
  * @param  string|null $encoding
  * @return string|null
  * @since  5.17
  */
-function char_at(string $str, int $index, string $encoding = null): string|null
+function char_at(string $string, int $index, string $encoding = null): string|null
 {
-    if ($index < 0 && -$index > mb_strlen($str, $encoding)) {
+    if ($index < 0 && -$index > mb_strlen($string, $encoding)) {
         return null;
     }
 
-    $char = mb_substr($str, $index, 1, $encoding);
+    $char = mb_substr($string, $index, 1, $encoding);
 
     return ($char != '') ? $char : null;
 }
@@ -300,19 +303,19 @@ function char_at(string $str, int $index, string $encoding = null): string|null
 /**
  * Get a character code by given index in multi-byte style.
  *
- * @param  string      $str
+ * @param  string      $string
  * @param  int         $index
  * @param  string|null $encoding
  * @return int|null
  * @since  5.17
  */
-function char_code_at(string $str, int $index, string $encoding = null): int|null
+function char_code_at(string $string, int $index, string $encoding = null): int|null
 {
-    if ($index < 0 && -$index > mb_strlen($str, $encoding)) {
+    if ($index < 0 && -$index > mb_strlen($string, $encoding)) {
         return null;
     }
 
-    $char = mb_substr($str, $index, 1, $encoding);
+    $char = mb_substr($string, $index, 1, $encoding);
 
     return ($char != '') ? mb_ord($char, $encoding) : null;
 }
