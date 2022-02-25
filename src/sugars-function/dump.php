f8aba3020ac795bd9ea0c4dc357bd6d52ceb5d7c
Kerem Güneş <qeremy@gmail.com>
2022-02-25 04:23:11 +0300

    Nope..

--- src/sugars-function/dump.php
+++ src/sugars-function/dump.php
@@ -6,24 +6,34 @@
 declare(strict_types=1);
 
 // Dirty debug/dump tools.. :(
-function _ps($s) {
-    if (is_null($s)) return 'NULL';
-    if (is_bool($s)) return $s ? 'TRUE' : 'FALSE';
-    return preg_replace('~\[(.+?):.+?:(private|protected)\]~', '[\1:\2]', print_r($s, true));
+if (!function_exists('_ps')) {
+    function _ps($s) {
+        if (is_null($s)) return 'NULL';
+        if (is_bool($s)) return $s ? 'TRUE' : 'FALSE';
+        return preg_replace('~\[(.+?):.+?:(private|protected)\]~', '[\1:\2]', print_r($s, true));
+    }
 }
-function _pd($s) {
-    ob_start(); var_dump($s); $s = ob_get_clean();
-    return preg_replace('~\["?(.+?)"?(:(private|protected))?\]=>\s+~', '[\1\2] => ', _ps(trim($s)));
+if (!function_exists('_pd')) {
+    function _pd($s) {
+        ob_start(); var_dump($s); $s = ob_get_clean();
+        return preg_replace('~\["?(.+?)"?(:(private|protected))?\]=>\s+~', '[\1\2] => ', _ps(trim($s)));
+    }
 }
-function pre($s, $e=0) {
-    echo "<pre>", _ps($s), "</pre>", "\n";
-    $e && exit;
+if (!function_exists('pre')) {
+    function pre($s, $e=0) {
+        echo "<pre>", _ps($s), "</pre>", "\n";
+        $e && exit;
+    }
 }
-function prs($s, $e=0) {
-    echo _ps($s), "\n";
-    $e && exit;
+if (!function_exists('prs')) {
+    function prs($s, $e=0) {
+        echo _ps($s), "\n";
+        $e && exit;
+    }
 }
-function prd($s, $e=0) {
-    echo _pd($s), "\n";
-    $e && exit;
+if (!function_exists('prd')) {
+    function prd($s, $e=0) {
+        echo _pd($s), "\n";
+        $e && exit;
+    }
 }
