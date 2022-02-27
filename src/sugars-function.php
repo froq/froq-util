246fc22f5b2fcc0915aa04c3991f87bb2e048c0d
Kerem Güneş <qeremy@gmail.com>
2022-02-27 11:52:10 +0300

    Add sugars-function/cast stuff.

--- src/sugars-function.php
+++ src/sugars-function.php
@@ -4,6 +4,7 @@ require 'sugars-function/_array.php';
 require 'sugars-function/_string.php';
 
 // Load other function files.
+require 'sugars-function/cast.php';
 require 'sugars-function/dump.php';
 require 'sugars-function/file.php';
 require 'sugars-function/http.php';
