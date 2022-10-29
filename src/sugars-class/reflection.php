c5ab35d9d0b87a37ac149820ba4ef91681e40887
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-10-29 20:51:34 +0300

    * sugars-class/reflection: fix type conflicts [drop "extends" use class_alias()].

--- src/sugars-class/reflection.php
+++ src/sugars-class/reflection.php
@@ -6,21 +6,42 @@
 declare(strict_types=1);
 
 /**
+ * Since these classes create & return (their) instances inside,
+ * typed properties become problematic as these types conflict.
+ * So here, we use class_alias() instead of extending bases.
+ * Eg: XReflectionType::of() => froq\reflection\ReflectionType
+ * @see Type::$reflection & Type::reflect().
+ */
+
+/**
  * Extensions.
  */
-class XReflectionObject extends froq\reflection\ReflectionObject {}
-class XReflectionClass extends froq\reflection\ReflectionClass {}
-class XReflectionClassConstant extends froq\reflection\ReflectionClassConstant {}
-class XReflectionProperty extends froq\reflection\ReflectionProperty {}
-class XReflectionMethod extends froq\reflection\ReflectionMethod {}
-class XReflectionFunction extends froq\reflection\ReflectionFunction {}
-class XReflectionParameter extends froq\reflection\ReflectionParameter {}
-class XReflectionType extends froq\reflection\ReflectionType {}
+// class XReflectionObject extends froq\reflection\ReflectionObject {}
+// class XReflectionClass extends froq\reflection\ReflectionClass {}
+// class XReflectionClassConstant extends froq\reflection\ReflectionClassConstant {}
+// class XReflectionProperty extends froq\reflection\ReflectionProperty {}
+// class XReflectionMethod extends froq\reflection\ReflectionMethod {}
+// class XReflectionFunction extends froq\reflection\ReflectionFunction {}
+// class XReflectionParameter extends froq\reflection\ReflectionParameter {}
+// class XReflectionType extends froq\reflection\ReflectionType {}
+class_alias(froq\reflection\ReflectionObject::class, XReflectionObject::class);
+class_alias(froq\reflection\ReflectionClass::class, XReflectionClass::class);
+class_alias(froq\reflection\ReflectionClassConstant::class, XReflectionClassConstant::class);
+class_alias(froq\reflection\ReflectionProperty::class, XReflectionProperty::class);
+class_alias(froq\reflection\ReflectionMethod::class, XReflectionMethod::class);
+class_alias(froq\reflection\ReflectionFunction::class, XReflectionFunction::class);
+class_alias(froq\reflection\ReflectionParameter::class, XReflectionParameter::class);
+class_alias(froq\reflection\ReflectionType::class, XReflectionType::class);
+
 
 /**
  * Additions.
  */
-class ReflectionTrait extends froq\reflection\ReflectionTrait {}
-class ReflectionInterface extends froq\reflection\ReflectionInterface {}
-class ReflectionNamespace extends froq\reflection\ReflectionNamespace {}
-class ReflectionCallable extends froq\reflection\ReflectionCallable {}
+// class ReflectionTrait extends froq\reflection\ReflectionTrait {}
+// class ReflectionInterface extends froq\reflection\ReflectionInterface {}
+// class ReflectionNamespace extends froq\reflection\ReflectionNamespace {}
+// class ReflectionCallable extends froq\reflection\ReflectionCallable {}
+class_alias(froq\reflection\ReflectionTrait::class, ReflectionTrait::class);
+class_alias(froq\reflection\ReflectionInterface::class, ReflectionInterface::class);
+class_alias(froq\reflection\ReflectionNamespace::class, ReflectionNamespace::class);
+class_alias(froq\reflection\ReflectionCallable::class, ReflectionCallable::class);
