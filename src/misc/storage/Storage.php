65c68b2cfa7591d498357ffe5941a2e5fb666b69
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-11-20 12:28:22 +0300

    Move misc.Storage => misc.storage.Storage.

--- src/misc/storage/Storage.php
+++ src/misc/storage/Storage.php
@@ -1,11 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright (c) 2015 · Kerem Güneş
  * Apache License 2.0 · http://github.com/froq/froq-util
  */
-declare(strict_types=1);
-
-namespace froq\util\misc;
+namespace froq\util\storage;
 
 use froq\common\interface\Arrayable;
 use froq\common\trait\DataAccessMagicOffsetTrait;
@@ -17,8 +15,8 @@ use froq\common\trait\DataAccessMagicOffsetTrait;
  * Note: For dynamic usage, access magic or offset methods
  * must be used, otherwise store/unstore methods can be used.
  *
- * @package froq\util\misc
- * @object  froq\util\misc\Storage
+ * @package froq\util\storage
+ * @object  froq\util\storage\Storage
  * @author  Kerem Güneş
  * @since   6.0
  */
