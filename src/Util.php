bb9cf0082d327ec103ce1c60fdc37144cbab3e91
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2022-12-03 03:22:19 +0300

    Make-ups.

--- src/Util.php
+++ src/Util.php
@@ -208,6 +208,7 @@ final /* fuckic static */ class Util extends \StaticClass
 
     /**
      * Make an array with given data input.
+     *
      * Note: must be used for arrays/iterables and stdClass or public var'ed objects.
      *
      * @param  array|object|null $data
@@ -255,6 +256,7 @@ final /* fuckic static */ class Util extends \StaticClass
 
     /**
      * Make an object with given data input.
+     *
      * Note: must be used for arrays/iterables and stdClass or public var'ed objects.
      *
      * @param  array|object|null $data
