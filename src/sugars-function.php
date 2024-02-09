4e802434d894f34fcc263d47430a561e87858624
Kerem Güneş <1508306+krmgns@users.noreply.github.com>
2024-02-09 22:54:06 +0300

    Add sugars-function/conv [decalp(),alpdec() & binhex(),hexbin()].

--- src/sugars-function.php
+++ src/sugars-function.php
@@ -10,6 +10,7 @@ require 'sugars-function/_string.php';
 
 // Load other function files.
 require 'sugars-function/cast.php';
+require 'sugars-function/conv.php';
 require 'sugars-function/dump.php';
 require 'sugars-function/file.php';
 require 'sugars-function/http.php';
---
+++ src/sugars-function/conv.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-util
+ */
+
+/**
+ * Convert a decimal / decimal string to alphabetical string.
+ *
+ * @param  int|string $number
+ * @param  int        $base
+ * @return string
+ */
+function decalp(int|string $number, int $base = 62): string
+{
+    $ret = convert_base($number, 10, $base);
+
+    return $ret;
+}
+
+/**
+ * Convert an alphabetical string to decimal / decimal string.
+ *
+ * @param  string $string
+ * @param  int    $base
+ * @param  bool   $cast
+ * @return int|string
+ */
+function alpdec(string $string, int $base = 62, bool $cast = true): int|string
+{
+    $ret = convert_base($string, $base, 10);
+
+    if ($cast && $ret <= PHP_INT_MAX) {
+        $ret = (int) $ret;
+    }
+
+    return $ret;
+}
+
+
+/**
+ * Aliases for those weirdos.
+ */
+function binhex(string $string): string       { bin2hex($string); }
+function hexbin(string $string): string|false { hex2bin($string); }
